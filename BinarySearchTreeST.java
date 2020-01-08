314f61c4672d392fa95a1ed7979729f64ce50640
jweir136 <jweir136@yahoo.com>
2020-01-08 12:29:27 -0500

    Add the correctly implemented binary search tree

--- BinarySearchTreeST.java
+++ BinarySearchTreeST.java
@@ -1,50 +1,50 @@
 public class BinarySearchTreeST<Key extends Comparable, Value> {
-  private Node root;
-  
+  public Node root;
+
   public BinarySearchTreeST() {
     this.root = null;
   }
 
-  public Value get(Key key) {
-    return search(key, this.root).value;
+  public void put(Key key, Value val) {
+    this.root = put(key, val, this.root);
   }
 
-  private Node search(Key key, Node node) {
+  private Node put(Key key, Value val, Node node) {
+    if (node == null)
+      return new Node(key, val);
+    else if (node.key.compareTo(key) == 0)
+      node.value = val;
+    else if (node.key.compareTo(key) < 0)
+      node.left = put(key, val, node.left);
+    else if (node.key.compareTo(key) > 0)
+      node.right = put(key, val, node.right);
+    
+    return node;
+  }
+
+  public Value get(Key key) {
+    return get(key, this.root);
+  }
+
+  private Value get(Key key, Node node) {
     if (node == null)
       return null;
     else if (node.key.compareTo(key) == 0)
-      return node;
-    else if (node.key.compareTo(key) > 0)
-      return search(key, node.left);
+      return node.value;
+    else if (node.key.compareTo(key) < 0)
+      return get(key, node.left);
     else
-      return search(key, node.right);
-  }
-
-  public void put(Key key, Value val) {
-    add(key, val, this.root);
-  }
-
-  private void add(Key key, Value val, Node node) {
-    if (node == null) {
-      node = new Node(key, val);
-      return;
-    } else if (node.key.compareTo(key) > 0) {
-      add(key, val, node.left);
-    } else if (node.key.compareTo(key) < 0) {
-      add(key, val, node.left);
-    }
+      return get(key, node.right);
   }
 
   private class Node {
-    public Node left, right;
     public Key key;
     public Value value;
+    public Node left, right;
 
     public Node(Key key, Value val) {
       this.key = key;
       this.value = val;
-      this.right = null;
-      this.left = null;
     }
   }
 }
